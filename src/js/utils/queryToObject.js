19fd3c07f53a60dd48d6e2debaf1ac98d7077204
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-07-02 16:51:59 -0400

    trying to add tests, fixed issue with queryToObject module was not using decodeURIComponent which it needed since its purpose is parsing url params

--- src/js/utils/queryToObject.js
+++ src/js/utils/queryToObject.js
@@ -1,7 +1,16 @@
+// Imports don't work in Jest due to import hoisting so I am unable to mock lodash
+// Must use common.js require which will be converted correctly due to babel-jest
+// /* global jest */
+// jest.dontMock('../../bower/lodash/lodash');
+// const _ = require('../../bower/lodash/lodash');
+
 import _ from 'lodash';
+
 /**
 * Takes a query string and converts it to an object
 * @param {string} querystring
 * @return {object}
 */
-export default (querystring) => _.zipObject(querystring.split('&').map(bit => bit.split('=')));
+export default (querystring) => {
+  return !querystring ? {} : _.zipObject(querystring.split('&').map(bit => bit.split('=').map(str => decodeURIComponent(str))));
+};
