a5b595dac3f91d0111790fb37fa731268862ebad
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-08-09 22:54:57 -0400

    testing using react-motions animations for basemap gallery

--- build.js
+++ build.js
@@ -10,6 +10,7 @@
 		// libs
 		'react': 'vendor/react/react.min',
 		'babel-polyfill': 'vendor/babel-polyfill/browser-polyfill',
+		'react-motion': 'vendor/react-motion/build/react-motion',
 		// Configured Packages
 		'js': 'js',
 		'vendor': 'vendor'
--- src/js/map/BasemapGallery.js
+++ src/js/map/BasemapGallery.js
@@ -2,9 +2,25 @@ import {MapActions as actions} from 'js/actions/MapActions';
 import {MAP as constants} from 'js/constants/AppConstants';
 import BasemapGalleryItem from 'js/map/BasemapGalleryItem';
 import MapStore from 'js/stores/MapStore';
+import {Spring} from 'react-motion';
 import React from 'react';
 
+/* Spring helper functions */
+let springConfig = [300, 50];
+let getEndValue = isOpen => { return { val: { height: isOpen ? 250 : 30, width: isOpen ? 210 : 30, opacity: isOpen ? 1.0 : 0 }, config: springConfig }; };
+let getStyle = delta => { return { height: `${delta.val.height}px`, width: `${delta.val.width}px`}; };
+let getDefaultValue = () => { return { val: { height: 30, width: 30, opacity: 0 } }; };
+/* BasemapGallery helper function */
 let getCurrentBasemap = () => MapStore.get(constants.basemap) || app.map.getBasemap();
+
+/* Old render content that does not use Spring animation
+<div className={'map-buttons basemap-gallery' + (this.state.open ? ' open' : '')} >
+  <div className='basemap-gallery-icon pointer' onClick={this.toggleGallery.bind(this)}>BG</div>
+  <ul className='basemap-gallery-list'>
+    {this.renderBasemapItems(this.props.basemaps)}
+  </ul>
+</div>
+*/
 
 export class BasemapGallery extends React.Component {
 
@@ -26,12 +42,16 @@ export class BasemapGallery extends React.Component {
 
   render () {
     return (
-      <div className={'map-buttons basemap-gallery' + (this.state.open ? ' open' : '')} >
-        <div className='basemap-gallery-icon pointer' onClick={this.toggleGallery.bind(this)}>BG</div>
-        <ul className='basemap-gallery-list'>
-          {this.renderBasemapItems(this.props.basemaps)}
-        </ul>
-      </div>
+      <Spring defaultValue={getDefaultValue()} endValue={getEndValue(this.state.open)}>
+        {delta =>
+          <div className='map-buttons basemap-gallery' style={getStyle(delta)}>
+            <div className='basemap-gallery-icon pointer' onClick={this.toggleGallery.bind(this)}>BG</div>
+            <ul className='basemap-gallery-list' style={{ opacity: delta.val.opacity }}>
+              {this.renderBasemapItems(this.props.basemaps)}
+            </ul>
+          </div>
+        }
+      </Spring>
     );
   }
 
