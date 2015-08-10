aabd95bf79b907a58c3cff062a8358f67be8eb28
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-08-10 17:07:57 -0400

    removed default value as it is not necessary

--- src/js/map/BasemapGallery.js
+++ src/js/map/BasemapGallery.js
@@ -9,7 +9,6 @@ import React from 'react';
 let springConfig = [300, 50]; // [stiffness, damping]
 let getEndValue = isOpen => { return { val: { height: isOpen ? 250 : 30, width: isOpen ? 210 : 30, opacity: isOpen ? 1.0 : 0 }, config: springConfig }; };
 let getStyle = delta => { return { height: `${delta.val.height}px`, width: `${delta.val.width}px`}; };
-let getDefaultValue = () => { return { val: { height: 30, width: 30, opacity: 0 } }; };
 /* BasemapGallery helper function */
 let getCurrentBasemap = () => MapStore.get(constants.basemap) || app.map.getBasemap();
 
@@ -33,7 +32,7 @@ export class BasemapGallery extends React.Component {
 
   render () {
     return (
-      <Spring defaultValue={getDefaultValue()} endValue={getEndValue(this.state.open)}>
+      <Spring endValue={getEndValue(this.state.open)}>
         {delta =>
           <div className='map-buttons basemap-gallery' style={getStyle(delta)}>
             <div className='basemap-gallery-icon pointer' onClick={this.toggleGallery.bind(this)}>BG</div>
